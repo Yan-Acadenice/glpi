18d358e21877ecb0092dbec874cf01a5d2317da4
yan-acadenice <yan@acadenice.fr>
2025-10-02 16:47:17 +0200

    glpi

---
+++ storage/glpi/config/config_db.php
@@ -0,0 +1,11 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = 'db:3306';
+   public $dbuser = 'glpi_app';
+   public $dbpassword = 'huieznji238U493Nncd%21%3FQSKSO';
+   public $dbdefault = 'glpi_mediashcool';
+   public $use_timezones = true;
+   public $use_utf8mb4 = true;
+   public $allow_datetime = false;
+   public $allow_signed_keys = false;
+}
